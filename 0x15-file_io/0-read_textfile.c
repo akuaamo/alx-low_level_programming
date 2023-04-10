ad2d679d43ec161d02b384d226b7b6d7b76db595
akuaamo <akuaacquah026@gmail.com>
2023-04-10 10:40:31 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,56 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and writes its contents to standard output
- * @filename: the name of the file to read
- * @letters: the maximum number of bytes to read from the file
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: the actual number of bytes read, or -1 on error
+ * Return: the actual number of letters it could read and print, or 0 if:
+ *         - filename is NULL
+ *         - file cannot be opened or read
+ *         - write fails or does not write the expected amount of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, ret;
-	ssize_t nread;
+	int fd;
+	ssize_t num_read, num_written;
 	char *buf;
 
 	if (filename == NULL)
-		return (ssize_t)-1;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (ssize_t)-1;
+		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
-		goto error;
+	{
+		close(fd);
+		return (0);
+	}
 
-	nread = read(fd, buf, letters);
-	if (nread == -1)
-		goto error;
+	num_read = read(fd, buf, letters);
+	if (num_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-	ret = write(STDOUT_FILENO, buf, nread);
-	if (ret == -1 || (size_t)ret != nread)
-		goto error;
+	num_written = write(STDOUT_FILENO, buf, num_read);
+	if (num_written == -1 || num_written != num_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
 	free(buf);
 	close(fd);
-	return nread;
 
-error:
-	free(buf);
-	close(fd);
-	return (ssize_t)-1;
+	return (num_written);
 }
-
