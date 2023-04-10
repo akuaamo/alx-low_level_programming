427c318b5bffd038520ee2c26001f8519421a32d
akuaamo <akuaacquah026@gmail.com>
2023-04-10 13:17:19 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,33 +6,41 @@
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
+ *_strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
  */
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include "main.h"
+size_t _strlen(char *str)
+{
+	size_t i;
 
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return (-1);
+	int fd;
+	ssize_t len = 0;
 
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        ssize_t len = strlen(text_content);
-        ssize_t bytes_written = write(fd, text_content, len);
-        if (bytes_written != len)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    close(fd);
-    return (1);
+	if (text_content != NULL)
+	{
+		len = write(fd, text_content, _strlen(text_content));
+		if (len == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
