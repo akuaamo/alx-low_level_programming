6bfece4413e6314d47d66a8fe01d36abbbee480d
akuaamo <akuaacquah026@gmail.com>
2023-04-10 09:39:55 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,52 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: Pointer to the name of the file to read
- * @letters: Number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: Number of letters read and printed, or 0 on failure
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+    int file_descriptor;
+    ssize_t len_read, len_written;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+        return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(file_descriptor);
+        return (0);
+    }
 
-	bytes_read = read(fd, buffer, letters);
-	close(fd);
+    len_read = read(file_descriptor, buffer, letters);
+    close(file_descriptor);
 
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+    if (len_read == -1)
+    {
+        free(buffer);
+        return (0);
+    }
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	free(buffer);
+    len_written = write(STDOUT_FILENO, buffer, len_read);
+    free(buffer);
 
-	if (bytes_written == -1 || bytes_read != bytes_written)
-		return (0);
+    if (len_read != len_written)
+        return (0);
 
-	return (bytes_read);
+    return (len_written);
 }
 
