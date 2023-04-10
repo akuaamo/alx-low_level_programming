10f945c98e4542fe89d10f2d9c292defd1f85dab
akuaamo <akuaacquah026@gmail.com>
2023-04-10 09:48:23 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,47 @@
 #include "main.h"
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - reads a text file and writes its contents to standard output
+ * @filename: the name of the file to read
+ * @letters: the maximum number of bytes to read from the file
  *
- * Return: actual number of letters it could read and print
+ * Return: the actual number of bytes read, or -1 on error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_descriptor;
-    ssize_t len_read, len_written;
-    char *buffer;
+	int fd, ret;
+	ssize_t nread;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (ssize_t)-1;
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (ssize_t)-1;
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(file_descriptor);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		goto error;
 
-    len_read = read(file_descriptor, buffer, letters);
-    close(file_descriptor);
+	nread = read(fd, buf, letters);
+	if (nread == -1)
+		goto error;
 
-    if (len_read == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	ret = write(STDOUT_FILENO, buf, nread);
+	if (ret == -1 || (size_t)ret != nread)
+		goto error;
 
-    len_written = write(STDOUT_FILENO, buffer, len_read);
-    free(buffer);
+	free(buf);
+	close(fd);
+	return nread;
 
-    if (len_read != len_written)
-        return (0);
-
-    return (len_written);
+error:
+	free(buf);
+	close(fd);
+	return (ssize_t)-1;
 }
 
