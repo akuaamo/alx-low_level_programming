903dd871ad58dbb914df58061fbd0d22e35ccbbf
akuaamo <akuaacquah026@gmail.com>
2023-04-10 13:07:22 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,10 @@ if (filename == NULL)
 return (-1);
 
 if (text_content != NULL)
-        {
-for (len = 0; text_content[len]; len++)
-                        ;
-        }
+{
+	for (len = 0; text_content[len]; len
+;
+}
 
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (fd == -1)
@@ -27,10 +27,10 @@ return (-1);
 
 w = write(fd, text_content, len);
 if (w == -1)
-        {
+{
 close(fd);
 return (-1);
-        }
+}
 
 close(fd);
 
